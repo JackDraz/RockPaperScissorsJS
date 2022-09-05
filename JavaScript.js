301653c9b25df20c89cd83ba1c67e99066030d5f
JackDraz <jackwillburnett@gmail.com>
2022-09-05 19:41:51 +0100

    Finished the v2 version of game

--- JavaScript.js
+++ JavaScript.js
@@ -1,83 +1,99 @@
-
 function getComputerChoice() {  /* pulls a random computer choice from 3 options*/ 
     let compChoices = ["Rock", "Paper", "Scissors"];
     let compChoice = compChoices[Math.floor(Math.random() * compChoices.length)];
     return compChoice.toUpperCase();
 }
 
-function getPlayerChoice() {    /*gets a player choice that is guaranteed to be ROCK, PAPER or SCISSORS*/
-    let playerChoices;
-    playerChoices = prompt("Please choose either: 'Rock', 'Paper' or 'Scissors: ");
-    playerChoices = playerChoices.toUpperCase();
+function checkWinner() {
+    //checks for winner and presents message if one is found
+    if (playerCount === 5 || computerCount === 5) {
+        if (playerCount > computerCount) {
+            finalScore.style.color = "green";
+            finalScore.textContent = `You win!! Final score is You: ${playerCount} - Computer ${computerCount}`;
+        } else {
+            finalScore.style.color = "red";
+            finalScore.textContent = `You lose... Final score is You: ${playerCount} - Computer ${computerCount}`;
+        }
 
-    while ((playerChoices !== "ROCK") && (playerChoices !== "PAPER") && (playerChoices !== "SCISSORS")) {
-        playerChoices = prompt("Invalid selection. Please choose either: 'Rock', 'Paper' or 'Scissors: ");
-        playerChoices = playerChoices.toUpperCase();
+        playerCount = 0; 
+        computerCount = 0;
+        runningScore.textContent = `Player: ${playerCount} Opponent: ${computerCount}`;
     }
-    return playerChoices;
+    console.log("test");
 }
 
 function roundRPS(computerChoice, playerChoice) {
-    const outcomeWin = `You win! ${playerChoice} beats ${computerChoice}!`
-    const outcomeLose = `You lose! ${computerChoice} beats ${playerChoice}...`
-    const outcomeDraw = `Draw!`
+    // Updated to take in click events as opposed to text
+    const outcomeWin = `You win! ${playerChoice} beats ${computerChoice}!`;
+    const outcomeLose = `You lose! ${computerChoice} beats ${playerChoice}...`;
+    const outcomeDraw = `Draw! Both players picked ${playerChoice}`;
 
+    //resets final score text
+    finalScore.textContent = "";
+
+    //round mechanics
     if (playerChoice === computerChoice) {
-        console.log(outcomeDraw);
-        return "draw";
+        roundOutcome.textContent = outcomeDraw;
+        return;
     }
 
     if (playerChoice === "ROCK") {
         if (computerChoice === "PAPER") {
-            console.log(outcomeLose);
-            return "lose";
-        } else
-            console.log(outcomeWin);
-            return "win";
+            roundOutcome.textContent = outcomeLose;
+            computerCount++;
+        } else {
+            roundOutcome.textContent = outcomeWin;
+            playerCount++;
         }
+    }
 
     if (playerChoice === "SCISSORS") {
         if (computerChoice === "ROCK") {
-            console.log(outcomeLose);
-            return "lose";
-        } else
-            console.log(outcomeWin);
-            return "win";
-
+            roundOutcome.textContent = outcomeLose;
+            computerCount++;
+        } else {
+            roundOutcome.textContent = outcomeWin;
+            playerCount++;
+        }
     }
 
     if (playerChoice === "PAPER") {
         if (computerChoice === "SCISSORS") {
-            console.log(outcomeLose);
-            return "lose";
-        } else
-            console.log(outcomeWin);
-            return  "win";
-    }
-}
-
-// plays a game that is first to 5
-
-function gameRPS() {
-    let computerCount = 0;
-    let playerCount = 0; 
-
-    while ((playerCount < 5) && (computerCount < 5)) {
-        let result;
-        console.log(`Player: ${playerCount} Opponent: ${computerCount}`)
-        result = roundRPS(getComputerChoice(), getPlayerChoice());
-        if (result === "win") {
-            playerCount++;
-        } else if (result === "lose") {
+            roundOutcome.textContent = outcomeLose;
             computerCount++;
         } else {
-            continue;
+            roundOutcome.textContent = outcomeWin;
+            playerCount++;
         }
     }
 
-    (playerCount > computerCount) ?
-    console.log(`You won! Final score: ${playerCount} - ${computerCount}`) :
-    console.log(`You lose! Final score: ${playerCount} - ${computerCount}`);   
+    runningScore.textContent = `Player: ${playerCount} Opponent: ${computerCount}`;
 }
 
-gameRPS()
+// adding DOM/Listeners
+function playerChoiceClick(e) {
+    roundRPS(getComputerChoice(), e.srcElement.classList.value);
+    checkWinner();
+}
+
+function gameRPS() {
+    // game structure/info targeting
+    const container = document.querySelector(".game-info");
+    container.appendChild(runningScore);
+    roundOutcome.textContent = "Choose your move...!"
+    container.appendChild(roundOutcome);
+    container.appendChild(finalScore);
+    
+    // creates array of nodes from HTML classes, using names as playerChoice variable
+    const choices = document.querySelectorAll("button");
+    choices.forEach(choice => choice.addEventListener("click", playerChoiceClick));
+}
+
+//game set up + method
+let roundOutcome = document.createElement('p');
+let runningScore = document.createElement('p');
+const finalScore = document.createElement('p');
+let computerCount = 0;
+let playerCount = 0; 
+
+gameRPS();
